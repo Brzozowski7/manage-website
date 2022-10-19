4e3281614c9b2dc3b334cffb63012c6569bc22c0
Krzysztof Brzozowski <krzysztofbrzozowski7@gmail.com>
2022-10-19 18:11:16 +0100

    base

---
+++ src/App/App.styles.ts
@@ -0,0 +1,16 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    padding:0;
+    margin:0;
+    box-sizing:border-box;
+}
+body{
+    
+}
+`;
+
+export const Wrapper = styled.div`
+
+`;
---
+++ src/App/App.tsx
@@ -0,0 +1,11 @@
+import { Wrapper, GlobalStyle } from "./App.styles";
+function App() {
+  return (
+    <Wrapper>
+      <GlobalStyle />
+      Hello
+    </Wrapper>
+  );
+}
+
+export default App;
